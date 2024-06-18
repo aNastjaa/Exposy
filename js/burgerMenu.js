cf3cae2bf9c27c32ffec4b80c9da0123034ea165
Nastja <caramelevaa@gmail.com>
2024-06-18 13:29:26 +0200

    Changes in CSS for header and fonts

---
+++ js/burgerMenu.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const burgerMenu = document.querySelector('.burger-menu');
+    const navLinks = document.querySelector('.nav-links');
+
+    burgerMenu.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+        burgerMenu.classList.toggle('active');
+    });
+});
