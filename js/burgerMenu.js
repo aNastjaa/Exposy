109ede8e2fcf520930f4e36e1edaed561a619aa0
Nastja <caramelevaa@gmail.com>
2024-06-20 13:09:23 +0200

    In this version i have made footer, eddited header, made a full working slider in JS. In general on this stage unlooged homepage view is done.

--- js/burgerMenu.js
+++ js/burgerMenu.js
@@ -1,9 +1,10 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function() {
     const burgerMenu = document.querySelector('.burger-menu');
     const navLinks = document.querySelector('.nav-links');
 
-    burgerMenu.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
+    burgerMenu.addEventListener('click', function() {
         burgerMenu.classList.toggle('active');
+        navLinks.classList.toggle('active');
     });
 });
+
